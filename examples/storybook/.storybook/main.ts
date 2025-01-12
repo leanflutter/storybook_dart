c76b5331d8e89dd045ff7adf918a8ee4a00b231d
李健莹 <lijy91@foxmail.com>
2025-01-12 08:30:24 +0800

    wip: Update storybook example

--- examples/storybook/.storybook/main.ts
+++ examples/storybook/.storybook/main.ts
@@ -3,17 +3,14 @@ import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/nextjs",
     options: {},
-  },
-  docs: {
-    autodocs: "tag",
   },
   staticDirs: ["../public"],
 };
